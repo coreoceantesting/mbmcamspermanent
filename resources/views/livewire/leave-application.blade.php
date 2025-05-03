07590d0bf121805c941b3e0a0db181dcb78d4481
aditya_pophale_c_o <shirsathpooja94@gmail.com>
2025-05-03 17:14:10 +0530

    approve status

--- resources/views/livewire/leave-application.blade.php
+++ resources/views/livewire/leave-application.blade.php
@@ -91,7 +91,7 @@
                             <td>{{ $melTaken }}</td>
                             <td style="font-size: 11px;">
                                 @foreach ($request->approvalHierarchy as $hierarchy)
-                                    <strong>{{ $loop->iteration }} Approver </strong> - {{ $hierarchy->status == 0 ? 'Pending' : 'Approved' }} <br>
+                                    <strong> @if($isAdmin) {{ $loop->iteration  }} Approver </strong> - @endif  {{ $hierarchy->status == 0 ? 'Pending' : 'Approved' }} <br>
                                 @endforeach
                             </td>
                             <td>{{ Str::limit($request->remark, 60) }}</td>
