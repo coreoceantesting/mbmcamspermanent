bee9e07f85e375c2124727c73af62244139ef620
sachin53704 <sachin53704@gmail.com>
2025-05-27 17:38:27 +0530

    display employee name

--- resources/views/admin/dashboard/employee-wise-report.blade.php
+++ resources/views/admin/dashboard/employee-wise-report.blade.php
@@ -180,7 +180,7 @@
                     {
                         if (!data.error2)
                         {
-                            $("#addForm input[name='name']").val(data.name);
+                            $("#addForm input[name='name']").val(data.employee.name);
                             $("#addForm input[name='ward']").val(data.employee?.ward?.name);
                             $("#addForm input[name='department']").val(data.employee?.department?.name);
                             $("#addForm input[name='class']").val(data.employee?.clas?.name);
